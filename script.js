4e94ff0c43be236b4456eb98f78d43b823ab5b21
Alex Coh <alex.coh@chasacademy.se>
2024-06-04 03:11:59 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  var menu = document.querySelector("#navbar");
+  menu.classList.toggle("active");
+}
+
+function toggleEasterEgg() {
+  var cat = document.querySelector(".easterEgg");
+  cat.classList.toggle("visible");
+}
